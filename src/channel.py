54e65a27d9c9e9a59069fbd30f9d110341df3d02
fkotyashichev <fkotyashichev@gmail.com>
2023-08-17 03:30:30 +0300

    fixed channel.py:

--- src/channel.py
+++ src/channel.py
@@ -2,6 +2,7 @@ import json
 import os
 from googleapiclient.discovery import build
 
+
 class Channel:
     """Класс для ютуб-канала"""
     api_key: str = os.getenv('YT_API_KEY')
@@ -9,9 +10,39 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+        self.title = self.get_info().get('items')[0].get('snippet').get('title')
+        self.description = self.get_info().get('items')[0].get('snippet').get('description')
+        self.url = f"http://www.youtube.com/channel/{self.get_info().get('items')[0].get('id')}"
+        self.subscriber_count = self.get_info().get('items')[0].get('snippet').get('subscriberCount')
+        self.video_count = self.get_info().get('items')[0].get('statistics').get('videoCount')
+        self.viewCount = self.get_info().get('items')[0].get('statistics').get('viewCount')
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @channel_id.setter
+    def chanel_id(self, channel_id):
+        self.__channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel = Channel.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    def get_info(self):
+        channel = Channel.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        return channel
+
+    def to_json(self, title):
+        """Запись полученной информации с сайта в файл"""
+        with open(title, 'w') as f:
+            json.dump(self.get_info(), f, indent=3)
+
+    @classmethod
+    def get_service(cls):
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
