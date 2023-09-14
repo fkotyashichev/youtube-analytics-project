03b21347c41454227a655486e4b3b68bfc705660
fkotyashichev <fkotyashichev@gmail.com>
2023-09-14 12:31:18 +0300

    fixed playlist.py: add new def

--- src/playlist.py
+++ src/playlist.py
@@ -1,6 +1,7 @@
 import os
 from googleapiclient.discovery import build
-import datetime
+from datetime import timedelta
+import isodate
 
 
 class PlayList:
@@ -17,3 +18,27 @@ class PlayList:
         self.videos = youtube.videos().list(part='contentDetails,statistics', id=','.join(self.video_ids)).execute()
         self.title = self.playlist['items'][0]['snippet']['title']
         self.url = f"https://www.youtube.com/playlist?list={self.playlist_id}"
+
+    @property
+    def total_duration(self):
+        duration_ = timedelta()
+        total_duration_ = timedelta()
+        for i in range(len(self.videos)):
+            iso_8601_duration = self.videos['items'][i]['contentDetails']['duration']
+            duration_ = isodate.parse_duration(iso_8601_duration)
+            total_duration_ += duration_
+        return total_duration_
+
+    def total_seconds(self):
+        return self.total_duration.seconds
+
+    def show_best_video(self):
+        videos = {}
+        id_ = None
+        likes = 0
+        for i in range(len(self.playlist_video)):
+            videos[self.videos['items'][i]['statistics']['likeCount']] = self.playlist_video['items'][i]
+            if likes < int(self.videos['items'][i]['statistics']['likeCount']):
+                likes = int(self.videos['items'][i]['statistics']['likeCount'])
+                id_ = self.videos['items'][i]['id']
+        return f"https://youtu.be/{id_}"
